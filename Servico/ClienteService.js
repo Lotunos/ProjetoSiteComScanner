db88fc3ce1934cea5ef9658f3a4a591df36238f9
José Álvaro Coelho Alves da Costa <jose-alvaro-coelho@hotmail.com>
2026-01-09 09:59:32 -0300

    Atualização dos registros

--- Servico/ClienteService.js
+++ Servico/ClienteService.js
@@ -9,11 +9,19 @@ export async function buscarCliente(cpf,senha){
 }
 export async function criarCliente(nome,cpf,senha) {
     //TODO: camadas de verificação devem ser aplicadas aqui
-    //TODO: Henrique
     const verificar = await objeto.verificarCPF(cpf);
     if(verificar != "ok"){
+        alert("CPF já cadastrado");//TODO: Trocar este alert
         return null; 
     }
     return await objeto.criarCliente(nome,cpf,senha);
     
+}
+export async function atualizarDados(cpf,senha){
+    const cliente = await objeto.atualizarDados(cpf,senha);
+    if(cliente == "ok"){
+        alert("CPF não encontrado");
+        return null;
+    }
+    return cliente;
 }
