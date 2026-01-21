dee245b328c005e45fad22144968d275ca1132b3
joao gabriel <joaogariel10@gmail.com>
2026-01-21 20:51:32 -0300

    adição dos arquivos menuAdmin e modificação do clienteservice

--- Servico/ClienteService.js
+++ Servico/ClienteService.js
@@ -1,27 +1,23 @@
-import * as objeto from "../DAO/ClienteDAO.js";
+import * as objeto from "../Servico/ClienteService.js";
 export async function buscarCliente(cpf,senha){
     const cliente = await objeto.buscarCliente(cpf,senha);
     if(cliente == null){
-        alert("Cliente não encontrado"); //TODO: adicionar uma marcação diferente de um alert
+        return null;//TODO: tratar este null
+    }
+   window.location.href = "../MenuAdmin/MenuAdmin.html";
+}
+export async function criarCliente(nome,cpf,senha){
+    const verificar = await objeto.criarCliente(nome,cpf,senha);
+    //TODO, isso precisa de uma validação
+    if(verificar == null){
         return null;
     }
-    return cliente;
-}
-export async function criarCliente(nome,cpf,senha) {
-    //TODO: camadas de verificação devem ser aplicadas aqui
-    const verificar = await objeto.verificarCPF(cpf);
-    if(verificar == "ok"){
-        alert("CPF já cadastrado");//TODO: Trocar este alert
-        return null; 
-    }
-    return await objeto.criarCliente(nome,cpf,senha);
-    
+    window.location.href = "../Login/Login.html";
 }
 export async function atualizarDados(cpf,senha){
-    const cliente = await objeto.atualizarDados(cpf,senha);
-    if(cliente == "ok"){
-        alert("CPF não encontrado");
+    const verificar = await objeto.atualizarDados(cpf,senha);
+    if(verificar != null){
         return null;
     }
-    return cliente;
+    window.location.href = "../Login/Login.html"
 }
